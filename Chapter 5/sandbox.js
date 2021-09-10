87fa6a47727faf3ff0feb66910687375f6a246ba
csquared92 <cclementous@gmail.com>
2021-09-10 06:50:09 -0700

    creating functions

---
+++ Chapter 5/sandbox.js
@@ -0,0 +1,14 @@
+// function declaration
+function greet(){
+    console.log("What's Good pimp");
+}
+
+// function expression
+
+const speak = function(){
+    console.log("It's gonna be a good day");
+};
+
+//greet();
+
+speak();
