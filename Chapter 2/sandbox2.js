676401b61d10bb6c6a3b4816243519caaa70eeff
clemmyclem <cclementous@rocketlawyer.com>
2021-09-10 06:09:11 -0700

    organizing files and folders

---
+++ Chapter 2/sandbox2.js
@@ -0,0 +1,51 @@
+// common strings methods
+
+//let email  = 'cclementous@gmail.com';
+
+//let result = email.lastIndexOf('n');
+
+//let result = email.substr(4,10);
+
+//replace method
+
+//let result = email.replace('m', 'w');
+//console.log(result);
+
+//Numbers in JavaScript
+
+let radius = 10;
+const pi = 3.14;
+console.log(radius, pi);
+
+// Maath operators +, -, *, / ** , %
+
+//console.log(10/2);
+
+//let result = radius % 3;
+
+// the formula to working out the area to a circle
+
+//let result = pi * radius**2;
+
+// order of operations 
+//B (Brackets) I(Indcies)  D(Division) M(Multipication) A(Adding) S(Subtracting)
+
+//let result = 5 * (10-3)**2;
+//console.log(result);
+
+// how to count the number of like on a blog
+
+let likes = 10;
+
+// //likes++;
+
+
+// likes += 10;
+// console.log(likes);
+
+// Nan - Not a number
+
+//console.log(5/ 'hello');
+
+let result  = 'The blog has ' + likes + ' likes';
+console.log(result);
